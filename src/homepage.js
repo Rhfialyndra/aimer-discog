861502d6cfe3e9eb1344b58540b5d41a5e2caf40
not4saken <87106378+Rhfialyndra@users.noreply.github.com>
2022-03-13 08:43:00 +0700

    Update homepage.js

--- src/homepage.js
+++ src/homepage.js
@@ -154,10 +154,10 @@ function Home() {
                 </div>
 
                 <section className="livePerformance-wrapper text-center">
-                        <h1 className="text-3xl my-16 md:mb-28 md:text-7xl text-center font-Montserrat font-bold text-pink-700 subpixel-antialiased filter drop-shadow-xl gray-200 select-none">
+                        <h1 className="text-3xl my-16 md:text-7xl text-center font-Montserrat font-bold text-pink-700 subpixel-antialiased filter drop-shadow-xl gray-200 select-none">
                             Live Performance
                         </h1>
-                        <h1 className="text-xl text-center text-red-700 drop-shadow-xl">This project is on halt due to lack of consistent resource and design. will be back in the future</h1>
+                        <h1 className="text-2xl font-bold text-center text-red-700 drop-shadow-xl">This project is on halt due to lack of consistent resource and design. will be back in the future</h1>
                         
                         <div>
 
